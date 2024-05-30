dcf317980e9103216fc77aa6a263658d4d06436d
onur <onuremiroglu00@gmail.com>
2024-05-30 14:43:48 +0300

    veritabanı okuma

--- app/src/main/java/com/onur/finalodevi/MainActivity2_34.java
+++ app/src/main/java/com/onur/finalodevi/MainActivity2_34.java
@@ -4,6 +4,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,12 +15,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2_34 extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+public class MainActivity2_34 extends AppCompatActivity {
+    private FirebaseDatabase veritabani;
+    private DatabaseReference veriyolu;
+
+    private ListView icerikliste;
+    ArrayList<String> liste;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main234);
+        icerikliste=findViewById(R.id.kayitlist);
+
+
+            liste = new ArrayList<>();
+            adapter = new ArrayAdapter(MainActivity2_34.this,android.R.layout.simple_list_item_1,liste);
+
+            veriyolu = FirebaseDatabase.getInstance().getReference("Final_projesi");
+            // My top posts by number of stars
+            veriyolu.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot veriler) {
+                    for (DataSnapshot sonveri: veriler.getChildren()) {
+                        String deger = sonveri.getValue().toString();
+
+                        liste.add(deger);
+
+                    }
+                    icerikliste.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(MainActivity2_34.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
     }
     public void urundetay (View view) {
         Intent gecis = new Intent (this, UrunDetay_34.class);
--- app/src/main/java/com/onur/finalodevi/YeniUrunKaydi_34.java
+++ app/src/main/java/com/onur/finalodevi/YeniUrunKaydi_34.java
@@ -1,6 +1,11 @@
 package com.onur.finalodevi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 public class YeniUrunKaydi_34 extends AppCompatActivity {
 
+        EditText txturunno, txturunadi,txtmodelkodu,txtsiparisadedi;
+        Button verikayit;
+    FirebaseAuth kimlik;
+    DatabaseReference veritabani;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_yeni_urun_kaydi34);
+        txturunno=findViewById(R.id.txtUrunNo);
+        txturunadi=findViewById(R.id.txtUrunAdi);
+        txtmodelkodu=findViewById(R.id.txtModelKodu);
+        txtsiparisadedi=findViewById(R.id.txtSiparisAdedi);
+        kimlik=FirebaseAuth.getInstance();
+        veritabani= FirebaseDatabase.getInstance().getReference("Final_projesi");
+
+    }
+    public void btngrdn (View view) {
+        Intent gecis = new Intent (this, MainActivity2_34.class);
+        startActivity(gecis);
+    }
+    public void verikayit(View view){
+        String no=txturunno.getText().toString();
+        String urunadi=txturunadi.getText().toString();
+        String modelkodu=txtmodelkodu.getText().toString();
+        String siparisadedi=txtsiparisadedi.getText().toString();
+        UUID uuid=UUID.randomUUID();
+        String ykayit=uuid.toString();
+        if(!no.isEmpty()&&!urunadi.isEmpty()&&!modelkodu.isEmpty()&&!siparisadedi.isEmpty()){
+            DatabaseReference yenikayit=veritabani.child(ykayit);
+            yenikayit.child("urunno").setValue(no);
+            yenikayit.child("urunadi").setValue(urunadi);
+            yenikayit.child("modelkodu").setValue(modelkodu);
+            yenikayit.child("siparisadedi").setValue(siparisadedi);
+            Toast.makeText(this, "Veritabanı Kayıt İşlemi Başarılı!", Toast.LENGTH_SHORT).show();
+
+                Intent gecis = new Intent (this, YeniUrunKaydi_34.class);
+                startActivity(gecis);
+
+        }else{
+            Toast.makeText(this, "Alanlar boş bırakılamaz", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
